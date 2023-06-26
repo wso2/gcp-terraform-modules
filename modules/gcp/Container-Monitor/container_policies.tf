1757ac126dc803358ef29f0b27b2351a7b72a250
rasanjaya85 <rlsubasinghe85@gmail.com>
2023-06-26 12:06:08 +0530

    Added the WSO2 banner

--- modules/gcp/Container-Monitor/container_policies.tf
+++ modules/gcp/Container-Monitor/container_policies.tf
@@ -1,3 +1,15 @@
+# -------------------------------------------------------------------------------------
+#
+# Copyright (c) 2023, WSO2 LLC. (http://www.wso2.com). All Rights Reserved.
+#
+# This software is the property of WSO2 LLC. and its suppliers, if any.
+# Dissemination of any information or reproduction of any material contained
+# herein in any form is strictly forbidden, unless permitted by WSO2 expressly.
+# You may not alter or remove any copyright or other notice from copies of this content.
+#
+# --------------------------------------------------------------------------------------
+
+
 resource "google_monitoring_alert_policy" "memory_container_alert_policy" {
   display_name = join("-", ["[ALERT POLICY] - GKE Container Memory", var.environment, var.scope])
   combiner     = "OR"
