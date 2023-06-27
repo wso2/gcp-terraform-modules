ff033153ff867b6950ccf018dac770bb8826c31a
rasanjaya85 <rlsubasinghe85@gmail.com>
2023-06-27 13:15:25 +0530

    Resolved conflict

--- modules/gcp/Bastion-VM/variables.tf
+++ modules/gcp/Bastion-VM/variables.tf
@@ -33,30 +33,17 @@ variable "bastion_ip_cidr_range" {
   description = "The IP CIDR range of the bastion subnet"
   type        = string
 }
-<<<<<<< Updated upstream
-variable "ssh_public_key_path" {
-  description = "The path to the ssh public key"
-  type        = string
-}
-variable "bastion_vm_machine_type" {
-  description = "The bastion VM machine type"
-=======
 variable  "ssh_public_key_path" {
   description = "The directory path to ssh public key"
   type        = string
 }
 variable "bastion_vm_machine_type" {
   description = "The Bastion VM machine type"
->>>>>>> Stashed changes
   type        = string
   default     = "e2-medium"
 }
 variable "boot_disk_image" {
-<<<<<<< Updated upstream
-  description = "The bastion vm boot disk image"
-=======
   description = "The Bastion VM boot disk image"
->>>>>>> Stashed changes
   type        = string
   default     = "ubuntu-os-cloud/ubuntu-2004-lts"
 }
