27028c7a6e1594d62eaf20790398b942c56403b6
janethavi <janethavishka@gmail.com>
2023-07-07 14:28:51 +0530

    [main-001] Add memorystore

---
+++ modules/gcp/Memorystore-Redis/memorystore_redis_.tf
@@ -0,0 +1,36 @@
+# -------------------------------------------------------------------------------------
+#
+# Copyright (c) 2023, WSO2 LLC. (http://www.wso2.com). All Rights Reserved.
+#
+# This software is the property of WSO2 LLC. and its suppliers, if any.
+# Dissemination of any information or reproduction of any material contained
+# herein in any form is strictly forbidden, unless permitted by WSO2 expressly.
+# You may not alter or remove any copyright or other notice from copies of this content.
+#
+# --------------------------------------------------------------------------------------
+
+resource "google_redis_instance" "memorystore_redis_instance" {
+  name                    = join("-", ["ms-redis", var.redis_name, var.environment])
+  project                 = var.project_name
+  tier                    = var.tier
+  memory_size_gb          = var.memory_size_gb
+  redis_version           = var.redis_version
+  region                  = var.region
+  location_id             = var.zone
+  reserved_ip_range       = var.reserved_ip_range
+  auth_enabled            = true
+  authorized_network      = var.vpc_id
+  transit_encryption_mode = "SERVER_AUTHENTICATION"
+  connect_mode            = "DIRECT_PEERING"
+  labels                  = var.labels
+}
+
+resource "google_dns_record_set" "a_record" {
+  project      = var.project_name
+  name         = join("", ["choreo-", var.environment, "-rate-limit.", var.internal_dns_name])
+  managed_zone = var.dns_managed_zone_name
+  type         = "A"
+  ttl          = "300"
+  rrdatas      = [google_redis_instance.memorystore_redis_instance.host]
+  depends_on   = [google_redis_instance.memorystore_redis_instance]
+}
---
+++ modules/gcp/Memorystore-Redis/variables.tf
@@ -0,0 +1,63 @@
+# -------------------------------------------------------------------------------------
+#
+# Copyright (c) 2023, WSO2 LLC. (http://www.wso2.com). All Rights Reserved.
+#
+# This software is the property of WSO2 LLC. and its suppliers, if any.
+# Dissemination of any information or reproduction of any material contained
+# herein in any form is strictly forbidden, unless permitted by WSO2 expressly.
+# You may not alter or remove any copyright or other notice from copies of this content.
+#
+# --------------------------------------------------------------------------------------
+
+variable "project_name" {
+  description = "The project name where the alert needs to be added"
+  type        = string
+}
+variable "tier" {
+  description = "The redis tier"
+  type        = string
+}
+variable "environment" {
+  description = "The redis environment"
+  type        = string
+}
+variable "redis_name" {
+  description = "The memorystore redis name"
+  type        = string
+}
+variable "redis_version" {
+  description = "The redis version"
+  type        = string
+}
+variable "region" {
+  description = "The redis region"
+  type        = string
+}
+variable "zone" {
+  description = "The redis zone"
+  type        = string
+}
+variable "vpc_id" {
+  description = "The vpc id"
+  type        = string
+}
+variable "labels" {
+  description = "The alert alignment period"
+  type        = map(string)
+}
+variable "memory_size_gb" {
+  description = "The memory size in GB"
+  type        = number
+}
+variable "reserved_ip_range" {
+  description = "The reserved ip range"
+  type        = string
+}
+variable "dns_managed_zone_name" {
+  description = "The DNS managed zone name"
+  type        = string
+}
+variable "internal_dns_name" {
+  description = "The dev internal dns name"
+  type        = string
+}
