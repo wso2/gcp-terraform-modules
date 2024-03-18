7002bbb90276db51eb86737bad1ec423245056e3
janethavi <janethavishka@gmail.com>
2024-03-18 17:16:03 +0530

    Add Static IP reservation

---
+++ modules/gcp/External-Static-IP-Reservation/static_ip.tf
@@ -0,0 +1,17 @@
+# -------------------------------------------------------------------------------------
+#
+# Copyright (c) 2024, WSO2 LLC. (http://www.wso2.com). All Rights Reserved.
+#
+# This software is the property of WSO2 LLC. and its suppliers, if any.
+# Dissemination of any information or reproduction of any material contained
+# herein in any form is strictly forbidden, unless permitted by WSO2 expressly.
+# You may not alter or remove any copyright or other notice from copies of this content.
+#
+# --------------------------------------------------------------------------------------
+
+resource "google_compute_address" "ip_address" {
+  name         =  join("-", [var.project_name, var.environment, var.ip_name, "ip"])
+  region       = var.project_region
+  ip_version   = var.ip_version
+  network_tier = var.network_tier
+}
