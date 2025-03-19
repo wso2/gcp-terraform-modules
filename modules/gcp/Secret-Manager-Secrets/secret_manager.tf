b87e619f85abcc5fe277eeeb35f01dd7c6588d91
janethavi <janethavishka@gmail.com>
2025-03-19 13:10:35 +0530

    Add secret manager

--- modules/gcp/Secret-Manager-Secrets/secret_manager.tf
+++ modules/gcp/Secret-Manager-Secrets/secret_manager.tf
@@ -1,6 +1,6 @@
 # -------------------------------------------------------------------------------------
 #
-# Copyright (c) 2023, WSO2 LLC. (http://www.wso2.com). All Rights Reserved.
+# Copyright (c) 2025, WSO2 LLC. (http://www.wso2.com). All Rights Reserved.
 #
 # This software is the property of WSO2 LLC. and its suppliers, if any.
 # Dissemination of any information or reproduction of any material contained
@@ -19,7 +19,7 @@ resource "google_secret_manager_secret" "secrets" {
   labels = var.labels
 }
 
-resource "google_secret_manager_secret_version" "secret-version" {
+resource "google_secret_manager_secret_version" "secret_version" {
   for_each    = { for secret in var.secrets : secret.name => secret }
   secret      = google_secret_manager_secret.secrets[each.value.name].id
   secret_data = each.value.secret_data
